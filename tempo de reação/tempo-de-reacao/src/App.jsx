3ff14396b87e546618effd7296fff7dc98ad8ea1
Rychardson <rychard.ribeiro.souza@hotmail.com>
2023-09-13 01:14:18 -0300

    fix: corrigindo medição do tempo de reação e adicionando contador de erros

--- tempo de reação/tempo-de-reacao/src/App.jsx
+++ tempo de reação/tempo-de-reacao/src/App.jsx
@@ -5,83 +5,62 @@ export default function App() {
 
   const cores = ['blue', 'red', 'yellow', 'green'];
 
-  var tempoAzul = [];
-  var tempoVermelho = [];
-  var tempoAmarelo = [];
-  var tempoVerde = [];
-
   const [corAtual, setCorAtual] = useState(cores[0]);
   const [tempoInicio, setTempoInicio] = useState(null);
   const [tempoReacao, setTempoReacao] = useState(null);
+  const [contador, setContador] = useState(0);
+  const [temposDeReacao, setTemposDeReacao] = useState([]);
+  const [temposPorCor, setTemposPorCor] = useState({
+    blue: [],
+    red: [],
+    yellow: [],
+    green: [],
+  });
+  const [mediaTempoReacao, setMediaTempoReacao] = useState(null);
 
   const handleKeyDown = (e) => {
-    
-    if(corAtual === 'green' && e.key === 'w'){
-      if(tempoInicio === null){
-        const inicio = performance.now();
-        setTempoInicio(inicio);
-      }else{
-        // Calcula o tempo de reação
-        const reacao = (performance.now() - tempoInicio) / 1000;
-        setTempoReacao(reacao);
-        tempoVerde.push(reacao);
-        setTempoInicio(null);
-        console.log("Vetor tempo verde: "+ tempoVerde);
-        console.log(`Tempo de reação verde: ${reacao.toFixed(2)} segundos`);
-      }
-    }
-   
-    if (corAtual === 'red' && e.key === 'a') {
+    if (corAtual === 'green' && e.key === 'w' ||
+        corAtual === 'red' && e.key === 'a' ||
+        corAtual === 'yellow' && e.key === 's' ||
+        corAtual === 'blue' && e.key === 'd') {
       if (tempoInicio === null) {
-        // Inicia a contagem de tempo
         const inicio = performance.now();
         setTempoInicio(inicio);
       } else {
-        // Calcula o tempo de reação
         const reacao = (performance.now() - tempoInicio) / 1000;
         setTempoReacao(reacao);
         setTempoInicio(null);
-        console.log(`Tempo de reação vermelho: ${reacao.toFixed(2)} segundos`);
-      }
-    }
+        console.log(`Tempo de reação da cor ${corAtual}: ${reacao.toFixed(2)} segundos`);
+        
+        // Adiciona o tempo de reação ao vetor
+        setTemposDeReacao((prevTempos) => [...prevTempos, reacao]);
 
-    if (corAtual === 'yellow' && e.key === 's') {
-      if (tempoInicio === null) {
-        // Inicia a contagem de tempo
-        const inicio = performance.now();
-        setTempoInicio(inicio);
-      } else {
-        // Calcula o tempo de reação
-        const reacao = (performance.now() - tempoInicio) / 1000;
-        setTempoReacao(reacao);
-        setTempoInicio(null);
-        console.log(`Tempo de reação amarelo: ${reacao.toFixed(2)} segundos`);
+        // Adiciona o tempo de reação ao vetor específico da cor atual
+        setTemposPorCor((prevTemposPorCor) => ({
+          ...prevTemposPorCor,
+          [corAtual]: [...prevTemposPorCor[corAtual], reacao.toFixed(2)],
+        }));
+        
+        if (temposDeReacao.length === 5) {
+          // Calcula a média dos tempos de reação
+          const media = temposDeReacao.reduce((acc, tempo) => acc + parseFloat(tempo), 0) / 5;
+          console.log(`Média dos tempos de reação: ${media.toFixed(2)} segundos`);
+          setMediaTempoReacao(media); // Atualiza a média
+          setTemposDeReacao([]); // Limpa o vetor
+        }
       }
+    } else {
+      setContador((prevContador) => prevContador + 1);
+      console.log(contador);
     }
-
-    if (corAtual === 'blue' && e.key === 'd') {
-      if (tempoInicio === null) {
-        // Inicia a contagem de tempo
-        const inicio = performance.now();
-        setTempoInicio(inicio);
-      } else {
-        // Calcula o tempo de reação
-        const reacao = (performance.now() - tempoInicio) / 1000;
-        setTempoReacao(reacao);
-        setTempoInicio(null);
-        console.log(`Tempo de reação azul: ${reacao.toFixed(2)} segundos`);
-      }
-    }
-
-   };
+  };
 
   useEffect(() => {
     const interval = setInterval(() => {
-      // Escolhe uma cor aleatória do vetor
       const novaCor = cores[Math.floor(Math.random() * cores.length)];
       setCorAtual(novaCor);
-      setTempoReacao(null); // Reseta o tempo de reação ao mudar de cor
-    }, 3000); // 3 segundos
+      setTempoReacao(null);
+    }, 2000);
 
     return () => clearInterval(interval);
   }, []);
@@ -93,31 +72,29 @@ export default function App() {
     };
   }, [corAtual]);
 
-  const calcularMediaVerde = () => {
-    if (tempoVerde.length === 0) {
-      return 0;
-    }
-    const soma = tempoVerde.reduce((acc, tempo) => acc + tempo, 0);
-    return soma / tempoVerde.length;
-  };
-
-
   return (
-      <div className='container'>
+    <div className='container'>
+      <div className='styleDados'>
         <div className='medicao'>
           <label className='styleMedicao'>
-          Vermelho: tecle A
-          Amarelo: tecle S
-          Azul: tecle D
-          Verde: tecle W
+            Vermelho: tecle A
+            Amarelo: tecle S
+            Azul: tecle D
+            Verde: tecle W
           </label>
         </div>
-        <div className='bolas' style={{ backgroundColor: corAtual }}>
+        <div className='contador-erros'>
+          <p>Contagem de Erros: {contador} </p>
         </div>
       </div>
-    
+      <div className='bolas' style={{ backgroundColor: corAtual }}/>
+      <div className='tempos-por-cor'>
+        <p>Tempo Vermelho(segundos): {temposPorCor.red.join(', ')}</p>
+        <p>Tempo Amarelo(segundos): {temposPorCor.yellow.join(', ')}</p>
+        <p>Tempo Azul(segundos): {temposPorCor.blue.join(', ')}</p>
+        <p>Tempo Verde(segundos): {temposPorCor.green.join(', ')}</p>
+        <p>Média de Tempo de Reação: {mediaTempoReacao !== null ? mediaTempoReacao.toFixed(2) : '-'}</p>
+      </div>
+    </div>
   )
-
 }
-
-
