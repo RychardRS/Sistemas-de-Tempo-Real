70717882a11b9aed3d76455d2b4b1aab22f0087b
Rychardson <rychard.ribeiro.souza@hotmail.com>
2023-10-31 18:49:03 -0300

    fix: correção de algumas funcionalidades dos botões

--- Gerenciador - und 2/gerenciador-processos/src/App.jsx
+++ Gerenciador - und 2/gerenciador-processos/src/App.jsx
@@ -4,6 +4,8 @@ import './App.css';
 
 const App = () => {
   const [processes, setProcesses] = useState([]);
+  const [pidToKill, setPidToKill] = useState('');
+  const [selectedFunction, setSelectedFunction] = useState('SIGKILL'); // Valor padrão é SIGKILL
 
   useEffect(() => {
     axios.get('http://localhost:3001/processes')
@@ -15,6 +17,30 @@ const App = () => {
       });
   }, []);
 
+  const displayedProcesses = processes.slice(0, 10);
+
+  const killProcess = () => {
+    if (!pidToKill) {
+      console.error('Digite um PID válido.');
+      return;
+    }
+
+    // Utilize o valor de selectedFunction para determinar a funcionalidade
+    let signal = selectedFunction;
+
+    // ...
+
+    axios.post('http://localhost:3001/killProcess', { pid: pidToKill, valor: signal })
+      .then((response) => {
+        console.log(`Processo ${pidToKill} morto com sucesso.`);
+        // Limpar o estado após matar o processo, se desejado
+        setPidToKill('');
+      })
+      .catch((error) => {
+        console.error('Erro ao matar processo:', error);
+      });
+  };
+
   return (
     <div className="boxContainer">
       <div className="boxGerenciador">
@@ -23,72 +49,35 @@ const App = () => {
         </div>
 
         <div className="processList">
-  <table>
-    <thead>
-      <tr>
-        <th>PID</th>
-        <th>PPID</th>
-        <th>Nome do Processo</th>
-      </tr>
-    </thead>
-    <tbody>
-      {processes.map((process, index) => (
-        <tr key={index}>
-          <td>{process.pid}</td>
-          <td>{process.ppid}</td>
-          <td>{process.command}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
+          <table>
+            <thead>
+              <tr>
+                <th>PID</th>
+                <th>PPID</th>
+              </tr>
+            </thead>
+            <tbody>
+              {displayedProcesses.map((process, index) => (
+                <tr key={index}>
+                  <td>{process.pid}</td>
+                  <td>{process.ppid}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
 
         <div className="boxInputs">
           <div className="box2Input">
             <div className="boxGenericInput">
-              <label className="tittleInput">Título</label>
+              <label className="tittleInput">Digite o PID</label>
               <input
                 type="text"
                 name="tittle"
                 id="tittle"
-                placeholder="Digite o seu título..."
+                placeholder="Digite o PID do processo..."
                 className="genericInput"
-                maxLength="50"
-              />
-            </div>
-          </div>
-          <div className="box2Input">
-            <div className="boxGenericInput" style={{ marginTop: '-30px' }}>
-              <label className="tittleInput">Descrição</label>
-              <textarea
-                type="text"
-                name="descricao"
-                id="descricao"
-                placeholder="Digite sua descrição...."
-                className="textArea"
-                maxLength="100"
-              ></textarea>
-            </div>
-          </div>
-          <div className="box3Input">
-            <div className="boxDeadLine" style={{ width: '84%' }}>
-              <label className="tittleInput">Deadline</label>
-              <input
-                type="text"
-                name="deadline"
-                id="deadline"
-                className="genericInput"
-                maxLength="50"
-              />
-            </div>
-            <div className="boxDeadLineMobile">
-              <label className="tittleInput">Deadline</label>
-              <input
-                type="text"
-                name="deadline2"
-                id="deadline2"
-                className="genericInput"
-                maxLength="50"
+                onChange={(e) => setPidToKill(e.target.value)}
               />
             </div>
           </div>
@@ -96,24 +85,29 @@ const App = () => {
 
         <div className="boxButtons">
           <div className="genericButton">
-            <button className="styleButton" type="button">
+            <button className="styleButton" type="button" onClick={() => setSelectedFunction('SIGKILL')}>
               <h3 className="textButton">Kill</h3>
             </button>
           </div>
           <div className="genericButton">
-            <button className="styleButton" type="button">
+            <button className="styleButton" type="button" onClick={() => setSelectedFunction('SIGSTOP')}>
               <h3 className="textButton">Stop</h3>
             </button>
           </div>
           <div className="genericButton">
-            <button className="styleButton" type="button">
+            <button className="styleButton" type="button" onClick={() => setSelectedFunction('SIGCONT')}>
               <h3 className="textButton">Cont</h3>
+            </button>
+          </div>
+          <div className="genericButton">
+            <button className="styleButton" type="button" onClick={() => setSelectedFunction('SIGINT')}>
+              <h3 className="textButton">Interrupt</h3>
             </button>
           </div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
--- Gerenciador - und 2/servidor-processos/server.js
+++ Gerenciador - und 2/servidor-processos/server.js
@@ -3,11 +3,16 @@
 const express = require('express');
 const ps = require('ps-node');
 const cors = require('cors');
+const bodyParser = require('body-parser'); // Importe o bodyParser
 
 const app = express();
 const port = 3001; // Porta do servidor
 
 app.use(cors());
+
+// Use o bodyParser para analisar o corpo da solicitação
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/processes', (req, res) => {
   ps.lookup({}, (err, resultList) => {
@@ -24,6 +29,38 @@ app.get('/processes', (req, res) => {
 
     res.json(processes);
   });
+});
+
+app.post('/killProcess', (req, res) => {
+  const { pid, valor } = req.body;
+
+  // Verifique o valor da funcionalidade e envie o sinal apropriado para matar o processo
+  let signal;
+  switch (valor) {
+    case 19:
+      signal = 'SIGSTOP';
+      break;
+    case 18:
+      signal = 'SIGCONT';
+      break;
+    case 2:
+      signal = 'SIGINT';
+      break;
+    case 9:
+      signal = 'SIGKILL';
+      break;
+    default:
+      return res.status(400).send('Valor de funcionalidade inválido.');
+  }
+
+  try {
+    // Utilize o sinal para matar o processo
+    process.kill(pid, signal);
+    res.status(200).send(`Processo ${pid} morto com sucesso.`);
+  } catch (error) {
+    console.error('Erro ao matar processo:', error);
+    res.status(500).send('Erro ao matar processo.');
+  }
 });
 
 app.listen(port, () => {
